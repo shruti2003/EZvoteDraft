81f1221f66aa8db4b3bd6d6f70779bc4d029fdc4
Shruti Patel <shruti.p2003@gmail.com>
2020-06-23 16:40:41 -0500

    Initial Commit

---
+++ EZvoteDraft/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EZvoteDraft
+//
+//  Created by shruti on 6/23/20.
+//  Copyright © 2020 Shruti Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
